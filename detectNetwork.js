2355f6f730d487a9a050bcdd09274e92e0add1d4
dclay1983 <dclay1983@gmail.com>
2019-09-22 10:15:48 -0700

    Add China UnionPay to detectNetwork

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,38 +13,50 @@ var detectNetwork = function(cardNumber) {
   let len = cardNumber.length;
   let prefix = parseInt(cardNumber.slice(0, 2))
   
-  if (len === 14 && (prefix == 38 || prefix == 39)) {
+  if (len === 14 && (prefix === 38 || prefix === 39)) {
     // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
     return "Diner's Club";
-  } else if (len === 15 && (prefix == 34 || prefix == 37)) {
+  } else if (len === 15 && (prefix === 34 || prefix === 37)) {
     // The American Express network always starts with a 34 or 37 and is 15 digits long
     return "American Express";
-  }else if (len === 16 && (prefix >= 51 && prefix <= 55)) {
+  } else if (len === 16 && (prefix >= 51 && prefix <= 55)) {
     // The MasterCard network always starts with 51-55 and is 16, digits long
     return "MasterCard";
-  }else if ((len === 13 || len === 16 || len === 19) && (prefix >= 40 && prefix <= 49)) {
+  } else if (false) {
+    // Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19.
+    return "Switch";
+  } else if ((len === 13 || len === 16 || len === 19) && (prefix >= 40 && prefix <= 49)) {
     // The Visa network always starts with 4 and is 13, 16, or 19 digits long
     return "Visa";
-  } else {
-    if ((len === 16 || len === 19) && (prefix === 60 || prefix === 64 || prefix === 65)) {
+  } else if ((len === 16 || len === 19) && (prefix === 60 || prefix === 64 || prefix === 65)){
     // Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
-      if (prefix === 65) {
-        return "Discover";
-      } else {
-        prefix = parseInt(cardNumber.slice(0, 4));
-        if (prefix === 6011 || (prefix >= 6440 && prefix <= 6499)) {
-          return "Discover";
-        }
-      }
-    } else if ((len >= 12 && len <= 19) && (prefix == 50 || prefix == 63)) {
-      // Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
+    if (prefix === 65) {
+      return "Discover";
+    } else {
       prefix = parseInt(cardNumber.slice(0, 4));
-      if (prefix === 5018 || prefix === 5020 || prefix === 5038 || prefix === 6304) {
-          return "Maestro";
+      if (prefix === 6011 || (prefix >= 6440 && prefix <= 6499)) {
+        return "Discover";
       }
-
+    }
+  } else if ((len >= 12 && len <= 19) && (prefix == 50 || prefix == 63)) {
+    // Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
+    prefix = parseInt(cardNumber.slice(0, 4));
+    if (prefix === 5018 || prefix === 5020 || prefix === 5038 || prefix === 6304) {
+        return "Maestro";
+    }
+  } else if ((len >= 16 && len <= 19) && prefix === 62) {
+    // China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19.
+    prefix = parseInt(cardNumber.slice(0, 3));
+    if (prefix >= 624 && prefix <= 626) { 
+      return "China UnionPay"
+    }
+    prefix = parseInt(cardNumber.slice(0, 4));
+    if (prefix >= 6282 && prefix <= 6288) { 
+      return "China UnionPay"
+    }
+    prefix = parseInt(cardNumber.slice(0, 6));
+    if (prefix >= 622126 && prefix <= 622925) {
+      return "China UnionPay"
     }
   }
 };
-
-
